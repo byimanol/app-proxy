406a30cbcfd002573a56adfc78817dd1ad4bb2e3
byimanol <imanol5522@gmail.com>
2026-03-28 18:06:45 -0400

    aprita

--- app/src/main/java/com/socks5setter/GetProxyReceiver.java
+++ app/src/main/java/com/socks5setter/GetProxyReceiver.java
@@ -4,11 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
-import java.io.File;
 
 public class GetProxyReceiver extends BroadcastReceiver {
-    private static final String TAG = "GetProxyReceiver";
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
@@ -18,8 +15,9 @@ public class GetProxyReceiver extends BroadcastReceiver {
         String user = prefs.getString("user", "");
         boolean connected = prefs.getBoolean("connected", false);
 
-        Log.d(TAG, "=== PROXY STATUS ===");
-        Log.d(TAG, "host=" + host + " port=" + port + " user=" + user + " connected=" + connected);
-        Log.d(TAG, "===================");
+        String result = "host=" + host + " port=" + port + " user=" + user + " connected=" + connected;
+
+        setResultCode(1);
+        setResultData(result);
     }
 }
